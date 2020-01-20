eb07fc3471bf7a0f605a6ccc72788ef167a4d4d0
Tom Schneider <tom.schneider-github@sutomaji.net>
2020-01-20 19:00:42 +0100

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("LICENSE.md") as f:
 
 setup(
     name="pygti",
-    version="0.1.0",
+    version="0.1.1",
     description="access public transport information in hamburg, germany.",
     long_description=readme,
     long_description_content_type="text/markdown",
