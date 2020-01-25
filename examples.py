25339d84c545854e9935ac056b4f2c81cb3874b1
Tom Schneider <1506970+vigonotion@users.noreply.github.com>
2020-01-25 13:42:45 +0100

    switch to aiohttp

--- examples.py
+++ examples.py
@@ -1,52 +1,73 @@
+import asyncio
+
+import aiohttp
+from pygti.auth import Auth
+from pygti.const import *
 from pygti.gti import *
 
 print("To run the examples, enter your credentials for the GTI API.")
 gti_user = input("GTI Username: ")
 gti_pass = input("GTI Password: ")
 
-gti = GTI(gti_user, gti_pass)
 
-print("Example 1: init()")
-ir = gti.init()
-print(ir)
+async def main():
+    async with aiohttp.ClientSession() as session:
 
-print()
-print("Example 2: checkName()")
-cn = gti.checkName({"theName": {"name": "Wartenau"}})
-print(cn)
+        auth = Auth(session, gti_user, gti_pass)
+        gti = GTI(auth)
 
-print()
-print("Example 3.1: departureList()")
-dl = gti.departureList(
-    {
-        "station": {"name": "Wartenau", "id": "Master:10901", "type": "STATION"},
-        "time": {"date": "heute", "time": "jetzt"},
-        "maxList": 5,
-        "maxTimeOffset": 200,
-        "useRealtime": True,
-    }
-)
+        print("Example 1: init()")
+        ir = await gti.init()
+        print(ir)
 
-print(dl)
+        print()
+        print("Example 2: checkName()")
+        cn = await gti.checkName({"theName": {"name": "Wartenau"}})
+        print(cn)
 
-print()
-print("Example 3.2: departureList(), return filters")
-dl = gti.departureList(
-    {
-        "station": {"name": "Wartenau", "id": "Master:10901", "type": "STATION"},
-        "time": {"date": "heute", "time": "jetzt"},
-        "maxList": 5,
-        "maxTimeOffset": 200,
-        "useRealtime": True,
-        "returnFilters": True,
-    }
-)
+        print()
+        print("Example 3.1: departureList()")
+        dl = await gti.departureList(
+            {
+                "station": {
+                    "name": "Wartenau",
+                    "id": "Master:10901",
+                    "type": "STATION",
+                },
+                "time": {"date": "heute", "time": "jetzt"},
+                "maxList": 5,
+                "maxTimeOffset": 200,
+                "useRealtime": True,
+            }
+        )
 
-print(dl)
+        print(dl)
 
-print()
-print("Example 4: stationInformation()")
-si = gti.stationInformation(
-    {"station": {"name": "Wartenau", "id": "Master:10901", "type": "STATION"}}
-)
-print(si)
+        print()
+        print("Example 3.2: departureList(), return filters")
+        dl = await gti.departureList(
+            {
+                "station": {
+                    "name": "Wartenau",
+                    "id": "Master:10901",
+                    "type": "STATION",
+                },
+                "time": {"date": "heute", "time": "jetzt"},
+                "maxList": 5,
+                "maxTimeOffset": 200,
+                "useRealtime": True,
+                "returnFilters": True,
+            }
+        )
+
+        print(dl)
+
+        print()
+        print("Example 4: stationInformation()")
+        si = await gti.stationInformation(
+            {"station": {"name": "Wartenau", "id": "Master:10901", "type": "STATION"}}
+        )
+        print(si)
+
+
+asyncio.run(main())
