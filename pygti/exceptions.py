a15b77e61c5499fb4fb2217d055c418688ca41b3
Tom Schneider <tom.schneider-github@sutomaji.net>
2020-01-26 10:58:30 +0100

    Add exceptions

---
+++ pygti/exceptions.py
@@ -0,0 +1,33 @@
+class GTIError(Exception):
+    """An exception occured while using the GTI API."""
+
+    def __init__(self, return_code, error_text, error_dev_info):
+        self.return_code = return_code
+        self.error_text = error_text
+        self.error_dev_info = error_dev_info
+
+
+class InvalidAuth(GTIError):
+    """Exception raised if credentials are incorrent."""
+
+    pass
+
+
+class CheckNameTooMany(GTIError):
+    """Check Name returned too many hits.
+
+    Precise search term."""
+
+    pass
+
+
+class CommunicationError(GTIError):
+    """Error while connecting to backend."""
+
+    pass
+
+
+class RouteError(GTIError):
+    """Route could not be calculated."""
+
+    pass
