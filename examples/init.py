409a9b424daf9081f53b29f1b4726ec1d69df643
Tom Schneider <1506970+vigonotion@users.noreply.github.com>
2026-04-01 22:19:06 +0200

    Migrate to uv

---
+++ examples/init.py
@@ -0,0 +1,37 @@
+import asyncio
+import os
+import aiohttp
+
+from pygti.auth import Auth
+from pygti.gti import GTI
+
+GTI_USER = None
+GTI_PASS = None
+
+try:
+    from dotenv import load_dotenv
+
+    load_dotenv()
+except ImportError:
+    pass
+
+GTI_USER = os.getenv("GTI_USER")
+GTI_PASS = os.getenv("GTI_PASS")
+
+if not (GTI_USER and GTI_PASS):
+    print("To run the examples, enter your credentials for the GTI API.")
+    GTI_USER = input("GTI Username: ")
+    GTI_PASS = input("GTI Password: ")
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+
+        auth = Auth(session, GTI_USER, GTI_PASS)
+        gti = GTI(auth)
+
+        print("Example 1: init()")
+        ir = await gti.init()
+        print(ir)
+
+asyncio.run(main())
